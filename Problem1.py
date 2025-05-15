14f6ee4dabbc60529c1432dc98a841cc3ac8692c
Yash Sonkamble <yash.sonkamble2000@gmail.com>
2025-05-15 15:13:56 -0400

    Final Solution

--- Problem1.py
+++ Problem1.py
@@ -1,23 +1,41 @@
 """
-I first created a hashset using a Python set in the __init__ function. 
-Then, I implemented basic methods to add a key, remove a key, and 
-check if a key exists in the hashset.
+I implemented a hash set using the technique taught in the session.
 """
 class MyHashSet:
 
     def __init__(self):
-        self.hashset = set()
+        self.hashSet = [None] * 1000
+        self.buckets = 1000
+        self.bucketItems = 1000
+    
+    def hash_function1(self, value):
+        return value % self.buckets
+    
+    def hash_function2(self, value):
+        return value // self.bucketItems
         
     def add(self, key: int) -> None:
-        self.hashset.add(key)
-        
+        i = self.hash_function1(key)
+        j = self.hash_function2(key)
+        if self.hashSet[i] is None:
+            if i == 0:
+                self.hashSet[i] = [False] * (self.bucketItems + 1)
+            else:
+                self.hashSet[i] = [False] * self.bucketItems
+        self.hashSet[i][j] = True
+
     def remove(self, key: int) -> None:
-        self.hashset.remove(key)
-
+        i = self.hash_function1(key)
+        j = self.hash_function2(key)
+        if self.hashSet[i] is None:
+            return
+        self.hashSet[i][j] = False
+    
     def contains(self, key: int) -> bool:
-        return key in self.hashset
-
-
+        i = self.hash_function1(key)
+        j = self.hash_function2(key)
+        return self.hashSet[i] is not None and self.hashSet[i][j]
+        
 # Your MyHashSet object will be instantiated and called as such:
 # obj = MyHashSet()
 # obj.add(key)
--- Problem2.py
+++ Problem2.py
@@ -1,43 +1,39 @@
 """
-I implemented a hash set using the technique taught in the session.
+I created two lists: one for the regular stack and another for tracking minimum values. In the push function, elements are added to both stacks if needed to maintain the minimum. In pop, if the top element of the min stack matches the popped element, it's removed from both stacks.
 """
-class MyHashSet:
+class MinStack:
 
     def __init__(self):
-        self.hashSet = [None] * 1000
-        self.buckets = 1000
-        self.bucketItems = 1000
-    
-    def hash_function1(self, value):
-        return value % self.buckets
-    
-    def hash_function2(self, value):
-        return value // self.bucketItems
-        
-    def add(self, key: int) -> None:
-        i = self.hash_function1(key)
-        j = self.hash_function2(key)
-        if self.hashSet[i] is None:
-            if i == 0:
-                self.hashSet[i] = [False] * (self.bucketItems + 1)
-            else:
-                self.hashSet[i] = [False] * self.bucketItems
-        self.hashSet[i][j] = True
+        self.stack = []
+        self.minStack = []
 
-    def remove(self, key: int) -> None:
-        i = self.hash_function1(key)
-        j = self.hash_function2(key)
-        if self.hashSet[i] is None:
-            return
-        self.hashSet[i][j] = False
-    
-    def contains(self, key: int) -> bool:
-        i = self.hash_function1(key)
-        j = self.hash_function2(key)
-        return self.hashSet[i] is not None and self.hashSet[i][j]
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        if not self.minStack or val <= self.minStack[-1]:
+            self.minStack.append(val)
+
+    def pop(self) -> None:
+        if self.stack:
+            popElement = self.stack.pop()
+            if popElement == self.minStack[-1]:
+                self.minStack.pop()
+
+    def top(self) -> int:
+        if self.stack:
+            return self.stack[-1]
+        else:
+            return None
+
+    def getMin(self) -> int:
+        if self.minStack:
+            return self.minStack[-1]
+        else:
+            return None
         
-# Your MyHashSet object will be instantiated and called as such:
-# obj = MyHashSet()
-# obj.add(key)
-# obj.remove(key)
-# param_3 = obj.contains(key)
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
