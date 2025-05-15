f30d1098fa2619fcb7f9095c984599de7ce9f2ec
Yash Sonkamble <yash.sonkamble2000@gmail.com>
2025-05-15 14:19:41 -0400

    Updated Problem 1 Solution

--- Problem2.py
+++ Problem2.py
@@ -1,36 +1,41 @@
 """
-I implemented a hash set using an array of lists to handle collisions through chaining. In the add method, I calculate the hash and insert the key into the corresponding list if it's not already there. For remove and contains, I use the same hash to find the correct list and check or delete the key.
+I implemented a hash set using the technique taught in the session.
 """
 class MyHashSet:
 
     def __init__(self):
-        self.size = 10 ** 4
-        self.hash_table = [None] * self.size
-
-    def calculate_hash_value(self, key: int) -> int:
-        return key % self.size
+        self.hashSet = [None] * 1000
+        self.buckets = 1000
+        self.bucketItems = 1000
+    
+    def hash_function1(self, value):
+        return value % self.buckets
+    
+    def hash_function2(self, value):
+        return value // self.bucketItems
         
     def add(self, key: int) -> None:
-        hash_value = self.calculate_hash_value(key)
-        if self.hash_table[hash_value] == None:
-            self.hash_table[hash_value] = [key]
-        else:
-            self.hash_table[hash_value].append(key)
-        
+        i = self.hash_function1(key)
+        j = self.hash_function2(key)
+        if self.hashSet[i] is None:
+            if i == 0:
+                self.hashSet[i] = [False] * (self.bucketItems + 1)
+            else:
+                self.hashSet[i] = [False] * self.bucketItems
+        self.hashSet[i][j] = True
+
     def remove(self, key: int) -> None:
-        hash_value = self.calculate_hash_value(key)
-        if self.hash_table[hash_value] != None:
-            while key in self.hash_table[hash_value]:
-                self.hash_table[hash_value].remove(key)
-
+        i = self.hash_function1(key)
+        j = self.hash_function2(key)
+        if self.hashSet[i] is None:
+            return
+        self.hashSet[i][j] = False
+    
     def contains(self, key: int) -> bool:
-        hash_value = self.calculate_hash_value(key)
-        if self.hash_table[hash_value] == None:
-            return False
-        else:
-            return key in self.hash_table[hash_value]
-
-
+        i = self.hash_function1(key)
+        j = self.hash_function2(key)
+        return self.hashSet[i] is not None and self.hashSet[i][j]
+        
 # Your MyHashSet object will be instantiated and called as such:
 # obj = MyHashSet()
 # obj.add(key)
